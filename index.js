dc0f170297bae2e90f996ef36777cf8bc06e1581
clarkj99 <clark@stontech.net>
2019-11-24 22:50:36 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(games) {
+  const win = games.find(game => game.result == 'W')
+  return win ? win.year : undefined
+}
